5f07024f2e22c9f5c49932e2af67351cae068e0c
Daniel Eze <eze_d@yahoo.com>
2022-10-02 18:49:57 +0100

    Update Cleaning Data and reliability check.sql

--- Cleaning Data and reliability check.sql
+++ Cleaning Data and reliability check.sql
@@ -4,5 +4,14 @@
   FROM film
   GROUP BY title, release_year, description, length, rating
   HAVING COUNT (*) > 1;
-
 --no dublicates--
+
+--checking non-uniform values--
+  SELECT DISTINCT rating
+  FROM film;
+  
+  --update the values that representa a "G" ratingto be consistent--
+  UPDATE film
+  SET rating  = 'G'
+  WHERE rating IN ('gen', 'g', 'General')
+  
